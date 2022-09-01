d3a0ea77095d8def3666bc18c14d592f991db49c
CDiala <dialachibuzo@yahoo.com>
2022-09-01 22:23:58 +0100

    Fully developed the xl-card component.

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,19 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import graduation from "./images/graduation.png";
 import ExtraLargeCard from "./components/shared/ExtraLargeCard/ExtraLargeCard";
-import Bookmark from "./components/shared/Bookmark/Bookmark";
 
 function App() {
   return (
     <>
       <Router>
-        {/* <ExtraLargeCard
+        <ExtraLargeCard
           src={graduation}
           alt={"template"}
           header="hello header"
           body={`Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa
           et, omnis atque rerum vel alias harum. Atque maiores temporibus facere
           consequuntur perferendis eos, quod quia hic, harum ipsum unde iste.`}
-        /> */}
-        <Bookmark />
+        />
       </Router>
     </>
   );
--- client/src/components/shared/ExtraLargeCard/ExtraLargeCard.js
+++ client/src/components/shared/ExtraLargeCard/ExtraLargeCard.js
@@ -1,4 +1,5 @@
 import { trimString } from "../../../utils/formatter";
+import Bookmark from "../Bookmark/Bookmark";
 import ReadMore from "../ReadMore/ReadMore";
 import "./ExtraLargeCard.css";
 
@@ -8,12 +9,17 @@ const ExtraLargeCard = ({ src, alt, header, body }) => {
       <div className="xl-card-img-box">
         <img className="xl-card-img" src={src} alt={alt} />
       </div>
-      <div>
-        <h3 className="xl-card-header">{header}</h3>
-        <p>
-          {trimString(body, 350) + " "}
-          <ReadMore path={"/"} />
-        </p>
+      <div className="xl-card-details">
+        <div className="xl-card-text">
+          <h3 className="xl-card-header">{header}</h3>
+          <p>
+            {trimString(body, 350) + " "}
+            <ReadMore path={"/"} />
+          </p>
+        </div>
+        <div className="bookmark">
+          <Bookmark />
+        </div>
       </div>
     </div>
   );
