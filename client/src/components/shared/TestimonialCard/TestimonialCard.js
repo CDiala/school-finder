779ba2ffee98c9b56c5b1cade49a417981be2958
CDiala <dialachibuzo@yahoo.com>
2022-09-02 22:56:16 +0100

    Created the testimonial component.

---
+++ client/src/components/shared/TestimonialCard/TestimonialCard.js
@@ -0,0 +1,25 @@
+import "./TestimonialCard.css";
+
+const TestimonialCard = (props) => {
+  const { testimony, names, status, image } = props.testament;
+  return (
+    <>
+      <div className="testimonial-container">
+        <div>
+          <p className="testimony">{testimony}</p>
+        </div>
+        <div className="testimonial-card-bottom">
+          <div className="tst-crd">
+            <p className="testimony-name">{names}</p>
+            <p>{status}</p>
+          </div>
+          <div className="testimony-profile-container">
+            <img className="testimonial-profile" src={image} alt="" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TestimonialCard;
