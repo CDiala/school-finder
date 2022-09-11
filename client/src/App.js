35628ab7b461acbcbcc5152a4e5b838c2da93989
CDiala <dialachibuzo@yahoo.com>
2022-09-11 07:21:38 +0100

    Created the structure of FAQ component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,11 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import graduation from "./images/graduation.png";
-import ExtraLargeCard from "./components/shared/ExtraLargeCard/ExtraLargeCard";
-import TestimonialCard from "./components/shared/TestimonialCard/TestimonialCard";
-
-import { FeatureSchoolList } from "./components/shared/FeatureSchoolCard/FeatureSchoolCard";
-import ImageCarousel from "./components/shared/ImageCarousel/ImageCarousel";
-import StyledNumber from "./components/shared/StyledNumber/StyledNumber";
-import Button from "./components/shared/Button/Button";
-import ResearchSchoolSection from "./components/shared/RSCard/RSCard";
+import FAQ from "./components/shared/FAQ/FAQ";
 
 function App() {
-  const objCarousel = [1, 2, 3];
   return (
     <Router>
-      {/* <StyledNumber number={objCarousel[1]} note={"hi there"} /> */}
-      {/* <Button text={"Find School"} width={"268"} /> */}
-      <ResearchSchoolSection />
+      <FAQ />
     </Router>
   );
 }
