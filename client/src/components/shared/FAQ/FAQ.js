1d4dc245f467059bb74e2d808b56ab427346f780
CDiala <dialachibuzo@yahoo.com>
2022-09-12 23:55:04 +0100

    Added code that displays answer to FAQ.

--- client/src/components/shared/FAQ/FAQ.js
+++ client/src/components/shared/FAQ/FAQ.js
@@ -44,21 +44,36 @@ const faqData = [
   },
 ];
 
+const showAnswer = (id) => {
+  const arrows = document.querySelectorAll(".collapsible__arrow");
+  const answers = document.querySelectorAll(".faq-answer");
+  console.log(answers);
+};
+
 const FAQ = () => {
   return (
     <ul className="faq-container">
       {faqData.map((item, index) => {
         return (
-          <li key={index} className="faq-item">
+          <li key={index} id={index} className="faq-item">
             <div
               className="faq-question-container"
               style={{
                 paddingTop: `${index === 0 ? "0" : null} `,
                 paddingBottom: `${index === faqData.length - 1 ? "0" : null}`,
               }}
+              onClick={(e) => {
+                let parentID =
+                  e.target.id ||
+                  e.target.parentElement.id ||
+                  e.target.parentElement.parentElement.id;
+                // console.log(parentID);
+                showAnswer(parentID);
+              }}
             >
               <p className="faq-question">{item.question}</p>
               <svg
+                className="collapsible__arrow"
                 width="16"
                 height="16"
                 viewBox="0 0 16 16"
