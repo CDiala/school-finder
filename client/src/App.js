581f48a662faa83bf95709da137beef7e41ff9d7
CDiala <dialachibuzo@yahoo.com>
2022-09-16 22:02:33 +0100

    Implemented the article component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import FAQ from "./components/shared/FAQ/FAQ";
+import ArticleCard from "./components/shared/ArticleCard/ArticleCard";
 
 function App() {
   return (
     <Router>
-      <FAQ />
+      <ArticleCard />
     </Router>
   );
 }
