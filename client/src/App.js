9b6ce48e96bbef3c1b68dbc0eff4ed4061a68c9d
CDiala <dialachibuzo@yahoo.com>
2022-09-16 22:55:15 +0100

    Implemented the component to check for update.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import graduation from "./images/graduation.png";
 import ExtraLargeCard from "./components/shared/ExtraLargeCard/ExtraLargeCard";
+import Bookmark from "./components/shared/Bookmark/Bookmark";
+import ReadMore from "./components/shared/ReadMore/ReadMore";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
           body={`Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa
           et, omnis atque rerum vel alias harum. Atque maiores temporibus facere
           consequuntur perferendis eos, quod quia hic, harum ipsum unde iste.`}
+          readMore={<ReadMore path={"./"} />}
+          bookmark={<Bookmark />}
         />
       </Router>
     </>
