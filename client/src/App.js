7b4570c49872eaced1b0dd73d7a67f67e1292f36
CDiala <dialachibuzo@yahoo.com>
2022-09-17 14:30:01 +0100

    Modified the component.

--- client/src/App.js
+++ client/src/App.js
@@ -13,9 +13,8 @@ function App() {
           src={graduation}
           alt={"template"}
           header="hello header"
-          body={`Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa
-          et, omnis atque rerum vel alias harum. Atque maiores temporibus facere
-          consequuntur perferendis eos, quod quia hic, harum ipsum unde iste.`}
+          body={`Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa et, omnis atque rerum vel alias harum. Atque maiores temporibus facere consequuntur perferendis eos, quod quia hic, harum ipsum unde iste. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa et, omnis atque rerum vel alias harum. Atque maiores temporibus facere consequuntur perferendis eos, quod quia hic,`}
+          charLength={300}
           readMore={<ReadMore path={"./"} />}
           bookmark={<Bookmark />}
         />
