6e4e47bb4e37a1d22a3808de9f52d6a413b3a967
CDiala <dialachibuzo@yahoo.com>
2022-10-05 00:53:14 +0100

    Refactored App.js file to show login page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import FAQ from "./components/shared/FAQ/FAQ";
-import FeatureSchoolList from "./components/shared/FeatureSchoolCard/FeatureSchoolCard";
-import { faqData } from "./data/appData";
+import Header from "./components/shared/Header/Header";
+import Footer from "./components/shared/Footer/Footer";
+import { objLinkItems } from "./data/objLinks";
+import logo from "./images/logo.svg";
+import { Login } from "./pages/Login/Login";
 
 function App() {
   return (
     <Router>
-      <FAQ faqData={faqData} />
-      <FeatureSchoolList />
+      <Header itemsArray={objLinkItems} logo={logo} />
+      <Login />
+      <Footer itemsArray={objLinkItems} />
     </Router>
   );
 }
