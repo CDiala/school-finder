baa96c2eaac420e20af43aff03215fbe180d8602
CDiala <dialachibuzo@yahoo.com>
2022-10-05 00:30:55 +0100

    Separation of concerns

--- client/src/data/appData.js
+++ client/src/data/appData.js
@@ -73,3 +73,35 @@ export const schools = [
     alt: "Covenant University logo",
   },
 ];
+
+// xlCard Data
+export const xlCardData = [
+  {
+    header: "Study in Nigeria: EduGrant Scholarship for Excellence 2021",
+    body: "Edugrant is a non-governmental organization that aims at providing students easy access to educational opportunities. This scholarship will cover tuition fees, accommodation, and stipend (upkeep). It is renewable provided the recipient maintains a CGPA of 3.5 or its equivalent",
+  },
+  {
+    header: "Dare undergraduate schlarship scheme for Oyo indigens",
+    body: "As part of the activities marking the 55th birthday celebrations of the Honourable Minister for youth and sports development, Chief Sunday Akin Dare, we are pleased to announce scholarship opportunities for indigent undergraduate students of Oyo state, under the Dare Undergraduate Scholarship Scheme (DUGSS)",
+  },
+  {
+    header:
+      "2021 High-Potential Scholarships at MCI Enterpreneurial school, Austria",
+    body: "The High-Potential Scholarships is a 50% tuition fee scholarship opportunity that will be awarded to students who are seeking to pursue a degree program at MCI Entrepreneurial School, Austria ",
+  },
+  {
+    header:
+      "Corporate Scholarships at Wavecrest College of Hospitality, Nigeria 2021",
+    body: "The Wavecrest College of Hospitality is currently awarding a scholarship opportunity to individuals who are willing to pursue a degree program at the Institution. In line with this, the College is envisioned to train women to be competent professionals able to meet the challenges of the fast-developing world of Hospitality",
+  },
+  {
+    header:
+      "Study in Germany: DAAD Bavarian Goverment Scholarships 2021/2022 for Internatonal Students at Hochschule HOF",
+    body: "Edugrant is a non-governmental organization that aims at providing students easy access to educational opportunities. This scholarship will cover tuition fees, accommodation, and stipend (upkeep). It is renewable provided the recipient maintains a CGPA of 3.5 or its equivalent",
+  },
+  {
+    header:
+      "Pradap Kow Scholarships at National Institute of Education - Singapore, 2021",
+    body: "National Institute of Education – Singapore is awarding the Pradap Kow Scholarships to support exceptional students who are interested in studying a degree program at the Institution. The Pradap Kow Scholarship",
+  },
+];
