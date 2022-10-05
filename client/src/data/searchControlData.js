7bb486a62e0160acb6f37e5c416f481358ced0dd
CDiala <dialachibuzo@yahoo.com>
2022-10-05 22:20:28 +0100

    Added data for the search widget.

---
+++ client/src/data/searchControlData.js
@@ -0,0 +1,96 @@
+export const programList = [
+  {
+    inputFor: "program",
+    title: "Program",
+    list: [
+      {
+        value: "",
+        option: "program",
+      },
+      {
+        value: "undergraduate",
+        option: "Undergraduate",
+      },
+      {
+        value: "graduate",
+        option: "Graduate",
+      },
+      {
+        value: "post graduate",
+        option: "Post Graduate",
+      },
+      {
+        value: "doctorate",
+        option: "Doctorate",
+      },
+    ],
+  },
+  {
+    inputFor: "course",
+    title: "Course",
+    list: [
+      {
+        value: "",
+        option: "Choose course",
+      },
+      {
+        value: "science",
+        option: "Science",
+      },
+      {
+        value: "technology",
+        option: "Technology",
+      },
+      {
+        value: "arts",
+        option: "Arts",
+      },
+    ],
+  },
+  {
+    inputFor: "location",
+    title: "Location",
+    list: [
+      {
+        value: "",
+        option: "Enter location",
+      },
+      {
+        value: "USA",
+        option: "USA",
+      },
+      {
+        value: "Canada",
+        option: "Canada",
+      },
+      {
+        value: "Australia",
+        option: "Australia",
+      },
+    ],
+  },
+];
+
+export const searchNavList = [
+  "Explore schools",
+  "Compare schools",
+  "Find scholarship",
+];
+
+export const widgetLabel = [
+  {
+    id: 1,
+    page: "explore-schools",
+    title: "Search for schools by programs, course or location",
+  },
+  {
+    id: 2,
+    page: "explore-schools",
+    title: "Check if your preferred course is offered by this school",
+  },
+  {
+    id: 3,
+    page: "scholarship",
+    title: "Search for the best scholarship for you",
+  },
+];
