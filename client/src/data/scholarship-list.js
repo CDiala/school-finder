3b242c337726b3ca42642f77b0944d8ee1644611
CDiala <dialachibuzo@yahoo.com>
2022-11-10 23:53:23 +0100

    feat: Create scholarship list data

---
+++ client/src/data/scholarship-list.js
@@ -0,0 +1,51 @@
+import err404 from "../images/404.svg";
+import err500 from "../images/500.svg";
+
+export const scholarshipList = [
+  {
+    title: "Study in Nigeria: EduGrant Scholarship for Excellence 2021",
+    details:
+      "Edugrant is a non-governmental organization that aims at providing students easy access to educational opportunities. This scholarship will cover tuition fees, accommodation, and stipend (upkeep). It is renewable provided the recipient maintains a CGPA of 3.5 or its equivalent...Read More",
+    img: err404,
+    alt: "error404",
+  },
+  {
+    title: "Dare undergraduate schlarship scheme for Oyo indigens",
+    details:
+      "As part of the activities marking the 55th birthday celebrations of the Honourable Minister for youth and sports development, Chief Sunday Akin Dare, we are pleased to announce scholarship opportunities for indigent undergraduate students of Oyo state, under the Dare Undergraduate Scholarship Scheme (DUGSS)...Read More",
+    img: err500,
+    alt: "error500",
+  },
+  {
+    title:
+      "2021 High-Potential Scholarships at MCI Enterpreneurial school, Austria",
+    details:
+      "The High-Potential Scholarships is a 50% tuition fee scholarship opportunity that will be awarded to students who are seeking to pursue a degree program at MCI Entrepreneurial School, Austria ...Read More",
+    img: err500,
+    alt: "",
+  },
+  {
+    title:
+      "Corporate Scholarships at Wavecrest College of Hospitality, Nigeria 2021",
+    details:
+      "The Wavecrest College of Hospitality is currently awarding a scholarship opportunity to individuals who are willing to pursue a degree program at the Institution. In line with this, the College is envisioned to train women to be competent professionals able to meet the challenges of the fast-developing world of Hospitality...Read More",
+    img: err500,
+    alt: "",
+  },
+  {
+    title:
+      "Study in Germany: DAAD Bavarian Goverment Scholarships 2021/2022 for Internatonal Students at Hochschule HOF",
+    details:
+      "Edugrant is a non-governmental organization that aims at providing students easy access to educational opportunities. This scholarship will cover tuition fees, accommodation, and stipend (upkeep). It is renewable provided the recipient maintains a CGPA of 3.5 or its equivalent...Read More",
+    img: err500,
+    alt: "",
+  },
+  {
+    title:
+      "Pradap Kow Scholarships at National Institute of Education - Singapore, 2021",
+    details:
+      "Pradap Kow Scholarships at National Institute of Education - Singapore, 2021",
+    img: err500,
+    alt: "",
+  },
+];
