79b980a6cf1474149836a3cb4ac9f8fcf4fe25ad
CDiala <dialachibuzo@yahoo.com>
2022-11-14 22:05:20 +0100

    fix: Correct typo in scholarship list data

--- client/src/data/scholarship-list.js
+++ client/src/data/scholarship-list.js
@@ -9,15 +9,15 @@ export const scholarshipList = [
   {
     title: "Study in Nigeria: EduGrant Scholarship for Excellence 2021",
     details:
-      "Edugrant is a non-governmental organization that aims at providing students easy access to educational opportunities. This scholarship will cover tuition fees, accommodation, and stipend (upkeep). It is renewable provided the recipient maintains a CGPA of 3.5 or its equivalen.",
+      "Edugrant is a non-governmental organization that aims at providing students easy access to educational opportunities. This scholarship will cover tuition fees, accommodation, and stipend (upkeep). It is renewable provided the recipient maintains a CGPA of 3.5 or its equivalent.",
     img: sch_1,
     alt: "scholarship",
     path: "/",
   },
   {
-    title: "Dare undergraduate schlarship scheme for Oyo indigens",
+    title: "Dare undergraduate scholarship scheme for Oyo indigens",
     details:
-      "As part of the activities marking the 55th birthday celebrations of the Honourable Minister for youth and sports development, Chief Sunday Akin Dare, we are pleased to announce scholarship opportunities for indigent undergraduate students of Oyo state, under the Dare Undergraduate Scholarship Scheme (DUGSS.",
+      "As part of the activities marking the 55th birthday celebration of the Honourable Minister for youth and sports development, Chief Sunday Akin Dare, we are pleased to announce scholarship opportunities for indigent undergraduate students of Oyo state, under the Dare Undergraduate Scholarship Scheme (DUGSS).",
     img: sch_2,
     alt: "scholarship",
     path: "/",
@@ -35,7 +35,7 @@ export const scholarshipList = [
     title:
       "Corporate Scholarships at Wavecrest College of Hospitality, Nigeria 2021",
     details:
-      "The Wavecrest College of Hospitality is currently awarding a scholarship opportunity to individuals who are willing to pursue a degree program at the Institution. In line with this, the College is envisioned to train women to be competent professionals able to meet the challenges of the fast-developing world of Hospitalit.",
+      "The Wavecrest College of Hospitality is currently awarding a scholarship opportunity to individuals who are willing to pursue a degree program at the Institution. In line with this, the College is envisioned to train women to be competent professionals able to meet the challenges of the fast-developing world of Hospitality.",
     img: sch_4,
     alt: "scholarship",
     path: "/",
@@ -44,7 +44,7 @@ export const scholarshipList = [
     title:
       "Study in Germany: DAAD Bavarian Goverment Scholarships 2021/2022 for Internatonal Students at Hochschule HOF",
     details:
-      "Edugrant is a non-governmental organization that aims at providing students easy access to educational opportunities. This scholarship will cover tuition fees, accommodation, and stipend (upkeep). It is renewable provided the recipient maintains a CGPA of 3.5 or its equivalen.",
+      "Edugrant is a non-governmental organization that aims at providing students easy access to educational opportunities. This scholarship will cover tuition fees, accommodation, and stipend (upkeep). It is renewable provided the recipient maintains a CGPA of 3.5 or its equivalent.",
     img: sch_5,
     alt: "scholarship",
     path: "/",
@@ -53,7 +53,7 @@ export const scholarshipList = [
     title:
       "Pradap Kow Scholarships at National Institute of Education - Singapore, 2021",
     details:
-      "National Institute of Education – Singapore is awarding the Pradap Kow Scholarships to support exceptional students who are interested in studying a degree program at the Institution. The Pradap Kow Scholarshi.",
+      "National Institute of Education – Singapore is awarding the Pradap Kow Scholarships to support exceptional students who are interested in studying a degree program at the Institution. The Pradap Kow Scholarship.",
     img: sch_6,
     alt: "scholarship",
     path: "/",
