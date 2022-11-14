01106e2bde7e853cda4453e4439aab265be5b5ab
CDiala <dialachibuzo@yahoo.com>
2022-11-14 21:44:24 +0100

    fix: Remove unwanted data from scholarship list

--- client/src/data/scholarship-list.js
+++ client/src/data/scholarship-list.js
@@ -9,7 +9,7 @@ export const scholarshipList = [
   {
     title: "Study in Nigeria: EduGrant Scholarship for Excellence 2021",
     details:
-      "Edugrant is a non-governmental organization that aims at providing students easy access to educational opportunities. This scholarship will cover tuition fees, accommodation, and stipend (upkeep). It is renewable provided the recipient maintains a CGPA of 3.5 or its equivalent...Read More",
+      "Edugrant is a non-governmental organization that aims at providing students easy access to educational opportunities. This scholarship will cover tuition fees, accommodation, and stipend (upkeep). It is renewable provided the recipient maintains a CGPA of 3.5 or its equivalen.",
     img: sch_1,
     alt: "scholarship",
     path: "/",
@@ -17,7 +17,7 @@ export const scholarshipList = [
   {
     title: "Dare undergraduate schlarship scheme for Oyo indigens",
     details:
-      "As part of the activities marking the 55th birthday celebrations of the Honourable Minister for youth and sports development, Chief Sunday Akin Dare, we are pleased to announce scholarship opportunities for indigent undergraduate students of Oyo state, under the Dare Undergraduate Scholarship Scheme (DUGSS)...Read More",
+      "As part of the activities marking the 55th birthday celebrations of the Honourable Minister for youth and sports development, Chief Sunday Akin Dare, we are pleased to announce scholarship opportunities for indigent undergraduate students of Oyo state, under the Dare Undergraduate Scholarship Scheme (DUGSS.",
     img: sch_2,
     alt: "scholarship",
     path: "/",
@@ -26,7 +26,7 @@ export const scholarshipList = [
     title:
       "2021 High-Potential Scholarships at MCI Enterpreneurial school, Austria",
     details:
-      "The High-Potential Scholarships is a 50% tuition fee scholarship opportunity that will be awarded to students who are seeking to pursue a degree program at MCI Entrepreneurial School, Austria ...Read More",
+      "The High-Potential Scholarships is a 50% tuition fee scholarship opportunity that will be awarded to students who are seeking to pursue a degree program at MCI Entrepreneurial School, Austria.",
     img: sch_3,
     alt: "scholarship",
     path: "/",
@@ -35,7 +35,7 @@ export const scholarshipList = [
     title:
       "Corporate Scholarships at Wavecrest College of Hospitality, Nigeria 2021",
     details:
-      "The Wavecrest College of Hospitality is currently awarding a scholarship opportunity to individuals who are willing to pursue a degree program at the Institution. In line with this, the College is envisioned to train women to be competent professionals able to meet the challenges of the fast-developing world of Hospitality...Read More",
+      "The Wavecrest College of Hospitality is currently awarding a scholarship opportunity to individuals who are willing to pursue a degree program at the Institution. In line with this, the College is envisioned to train women to be competent professionals able to meet the challenges of the fast-developing world of Hospitalit.",
     img: sch_4,
     alt: "scholarship",
     path: "/",
@@ -44,7 +44,7 @@ export const scholarshipList = [
     title:
       "Study in Germany: DAAD Bavarian Goverment Scholarships 2021/2022 for Internatonal Students at Hochschule HOF",
     details:
-      "Edugrant is a non-governmental organization that aims at providing students easy access to educational opportunities. This scholarship will cover tuition fees, accommodation, and stipend (upkeep). It is renewable provided the recipient maintains a CGPA of 3.5 or its equivalent...Read More",
+      "Edugrant is a non-governmental organization that aims at providing students easy access to educational opportunities. This scholarship will cover tuition fees, accommodation, and stipend (upkeep). It is renewable provided the recipient maintains a CGPA of 3.5 or its equivalen.",
     img: sch_5,
     alt: "scholarship",
     path: "/",
@@ -53,7 +53,7 @@ export const scholarshipList = [
     title:
       "Pradap Kow Scholarships at National Institute of Education - Singapore, 2021",
     details:
-      "National Institute of Education – Singapore is awarding the Pradap Kow Scholarships to support exceptional students who are interested in studying a degree program at the Institution. The Pradap Kow Scholarship...Read More",
+      "National Institute of Education – Singapore is awarding the Pradap Kow Scholarships to support exceptional students who are interested in studying a degree program at the Institution. The Pradap Kow Scholarshi.",
     img: sch_6,
     alt: "scholarship",
     path: "/",
