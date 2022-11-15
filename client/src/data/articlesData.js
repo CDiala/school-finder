5e7a6924e234ad94318cf46c403aa86f7912db2d
CDiala <dialachibuzo@yahoo.com>
2022-11-15 21:33:07 +0100

    fix: Separation of concern on article card

---
+++ client/src/data/articlesData.js
@@ -0,0 +1,27 @@
+import src1 from "../images/graduation.png";
+import src2 from "../images/article_img_2.png";
+
+export const articles = [
+  {
+    src: src1,
+    alt: "article 1",
+    header: "Why study Medicine???",
+    body: `
+      When I was much younger, I had wanted to study medicine, but then when I grew up, I realized I had an abject fear of blood. Lol. Dont laugh at me please. I don’t know hen I was much younger, I had wanted to study medicine, but then when I grew up, I realized I had an abject fear of blood. Lol. Dont laugh at me please. I don’t know
+      `,
+    date: "September 6, 2021",
+    datetime: "2022-09-06",
+    path: "/",
+  },
+  {
+    src: src2,
+    alt: "I got the scholarship! Yaay!!",
+    header: "test header 2",
+    body: `
+      I guess you would be wondering why I am this excited. Let me give you gist. By the way, I came with ways on how you can easily get that scholarship. Oh yes, I am that nice. Lol. Back to the gist oo, I have
+      `,
+    date: "November 12, 2021",
+    datetime: "2021-11-12",
+    path: "/",
+  },
+];
