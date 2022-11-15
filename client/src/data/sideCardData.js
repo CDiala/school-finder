6140d4ad1e3ff6efef693539e89882f74c3be05f
CDiala <dialachibuzo@yahoo.com>
2022-11-15 22:03:44 +0100

    fix: Add more side card data

--- client/src/data/sideCardData.js
+++ client/src/data/sideCardData.js
@@ -1,27 +1,42 @@
 import src1 from "../images/graduation.png";
-import src2 from "../images/article_img_2.png";
+import src2 from "../images/medicine.png";
+import src3 from "../images/scholarship.png";
 
 export const articles = [
   {
     src: src1,
-    alt: "article 1",
-    header: "Why study Medicine???",
+    alt: "advertisement",
+    header: "UNILORIN begins sales of Post UTME forms!",
     body: `
-      When I was much younger, I had wanted to study medicine, but then when I grew up, I realized I had an abject fear of blood. Lol. Dont laugh at me please. I don’t know hen I was much younger, I had wanted to study medicine, but then when I grew up, I realized I had an abject fear of blood. Lol. Dont laugh at me please. I don’t know
+      The University of Ilorin has began the sales of UTME form amidst the prolonged ASUU strike. The institution was yesterday reprimanded by a host of other universities fully engaged in the protest to desist from the form sales, or face the consequences.
       `,
-    date: "September 6, 2021",
-    datetime: "2022-09-06",
+    date: "June 3, 2022",
+    datetime: "2022-06-03",
     path: "/",
+    type: "Advertisement",
   },
   {
     src: src2,
-    alt: "I got the scholarship! Yaay!!",
-    header: "test header 2",
+    alt: "article",
+    header: "Why study Medicine???",
     body: `
       I guess you would be wondering why I am this excited. Let me give you gist. By the way, I came with ways on how you can easily get that scholarship. Oh yes, I am that nice. Lol. Back to the gist oo, I have
       `,
     date: "November 12, 2021",
     datetime: "2021-11-12",
     path: "/",
+    type: "New Article",
+  },
+  {
+    src: src3,
+    alt: "article",
+    header: "I got the scholarship! Yaay!!",
+    body: `
+      I guess you would be wondering why I am this excited. Let me give you gist. By the way, I came with ways on how you can easily get that scholarship. Oh yes, I am that nice. Lol. Back to the gist oo, I have
+      `,
+    date: "November 12, 2021",
+    datetime: "2021-11-12",
+    path: "/",
+    type: "New Article",
   },
 ];
